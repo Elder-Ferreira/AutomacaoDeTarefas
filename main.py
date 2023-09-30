63c6cf15ca5000aad0d819f339f9729b49bba1c1
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-09-29 22:44:04 -0300

    Sinalizando ponto de click e automatizando login do usuário

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
 time.sleep(2)
+
+pyautogui.click(x=445, y=396)
+pyautogui.write("testeautomatizado@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("teste123")
+pyautogui.press("tab")
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.press("enter")
