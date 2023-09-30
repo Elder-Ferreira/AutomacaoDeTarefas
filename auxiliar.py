e3c34515bceba9afe8f868f600bc93a2d582d367
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-09-30 00:43:32 -0300

    Automatizando cadastro de produtos no sistema

--- auxiliar.py
+++ auxiliar.py
@@ -2,4 +2,11 @@ import pyautogui
 import time
 
 time.sleep(5)
+
 print(pyautogui.position())
+
+# pyautogui.scroll(500)
+# pyautogui.press("home")
+
+# for linha in tabela.index:
+# for coluna in tabela.columns:
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(2)
+time.sleep(3)
 
 pyautogui.click(x=445, y=396)
 pyautogui.write("testeautomatizado@gmail.com")
@@ -19,5 +19,42 @@ pyautogui.press("tab")
 pyautogui.write("teste123")
 pyautogui.press("tab")
 pyautogui.press("enter")
-time.sleep(2)
+time.sleep(3)
 pyautogui.press("enter")
+
+
+import pandas
+
+tabela = pandas.read_csv("produtos.csv")
+# print(tabela)
+
+for linha in tabela.index:
+    pyautogui.click(x=442, y=293)
+
+    codigo = tabela.loc[linha, "codigo"]
+    marca = tabela.loc[linha, "marca"]
+
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+    pyautogui.write(str(marca))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll(50000)
+
+
+# enviar_email()
