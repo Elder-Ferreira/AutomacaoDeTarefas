68e12ec48a44c734f236e56abebf6766dc4eed6d
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-09-29 22:37:53 -0300

    Automatizando inicialização do navegador

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+pyautogui.write(link)
+pyautogui.press("enter")
+time.sleep(2)
